237b67ea33ab2ee1602fc411adcb17243820ec70
Silvana Cecilia Moreira <silvanamoreira85@outlook.com>
2021-04-22 00:00:44 +0200

    configurate query to create pages dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const response = await graphql(`
+        query {
+            allDatoCmsRoom {
+                nodes {
+                    slug
+                }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    if (response.errors) {
+        reporter.panic("No results", response.errors);
+    }
+
+    const rooms = response.data.allDatoCmsRoom.nodes;
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve("./src/components/rooms"),
+            context: {
+                slug: room.slug
+            }
+        });
+    });
+}
