d02ff78b93b406b6bb36146b108bab1d5be9a528
Silvana Cecilia Moreira <silvanamoreira85@outlook.com>
2021-04-22 00:01:43 +0200

    change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Hotel`,
+    description: `Web created with Gatsby and GraphQL.`,
+    author: `SCM`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/logo.jpg`,
       },
     },
     {
