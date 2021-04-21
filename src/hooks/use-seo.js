61df74589644d1a58d70d763a96cb95275e96f6a
Silvana Cecilia Moreira <silvanamoreira85@outlook.com>
2021-04-22 01:09:37 +0200

    create seo hooks to control title and metadata

---
+++ src/hooks/use-seo.js
@@ -0,0 +1,20 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+const useSeo = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            datoCmsSite {
+                globalSeo {
+                    siteName
+                    fallbackSeo {
+                        title
+                        description
+                    }
+                }
+            }
+        }
+    `);
+    return data.datoCmsSite.globalSeo;
+};
+
+export default useSeo;
